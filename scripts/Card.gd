f29f70c84c6e0b58d6bb07ca1d3526b0456b52c8
Pete Goodwin <gitkraken@imekon.org>
2018-11-03 09:23:37 +0000

    adding flags to cards

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -5,6 +5,11 @@ onready var hit_label = $HitLabel
 onready var defense_label = $DefenseLabel
 onready var symbol = $Symbol
 
+export var is_rock : bool = false
+export var is_paper : bool = false
+export var is_scissors : bool = false
+export var is_special : bool = false
+
 func set_details(cost : int, hit : int, defense : int, visibility : Color):
 	cost_label.text = str(cost)
 	hit_label.text = str(hit)
